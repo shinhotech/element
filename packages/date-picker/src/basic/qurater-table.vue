075148530adc8133cbe306ae795556f68a20cb0c
zhangjingguo_19080088 <zhangjingguo@shinho.net.cn>
2020-12-04 17:19:20 +0800

    feat: :add: 新增季度组件

---
+++ packages/date-picker/src/basic/qurater-table.vue
@@ -0,0 +1,32 @@
+<!--
+ * @Descripttion: 季度组件面板
+ * @Author: 19080088
+ * @Date: 2020-11-24 10:49:51
+ * @LastEditors: 19080088
+ * @LastEditTime: 2020-12-04 16:12:27
+-->
+<template>
+  <table @click="handleQuraterTableClick">
+    <tbody></tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  mounted () {},
+  methods: {
+    handleQuraterTableClick (event) {
+      console.log('event: ', event);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
