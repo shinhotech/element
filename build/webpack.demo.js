6ab0056e2604e098b76e818bcd67b766ab01aa8f
zhangjingguo_19080088 <zhangjingguo@shinho.net.cn>
2021-03-09 18:02:10 +0800

    build: 更新配置

--- build/webpack.demo.js
+++ build/webpack.demo.js
@@ -11,7 +11,6 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const config = require('./config');
 
 const isProd = process.env.NODE_ENV === 'production';
-const outputPath = process.env.DOCS ? './docs' : './examples/shinho-sh-ui/';
 const isPlay = !!process.env.PLAY_ENV;
 
 const webpackConfig = {
@@ -20,7 +19,7 @@ const webpackConfig = {
     docs: './examples/entry.js'
   } : (isPlay ? './examples/play.js' : './examples/entry.js'),
   output: {
-    path: path.resolve(process.cwd(), outputPath),
+    path: path.resolve(process.cwd(), './docs'),
     publicPath: process.env.CI_ENV || '',
     filename: '[name].[hash:7].js',
     chunkFilename: isProd ? '[name].[hash:7].js' : '[name].js'
@@ -153,7 +152,7 @@ if (isProd) {
     cacheGroups: {
       vendor: {
         test: /\/src\//,
-        name: 'shinhotech/sh-ui',
+        name: 'sh-ui',
         chunks: 'all'
       }
     }
